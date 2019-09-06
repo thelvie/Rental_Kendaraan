bcfa16a84e993be82e2ca9b5a8f4ef3e99c12082
Delfi Arsita <24351719+thelvie@users.noreply.github.com>
2019-09-06 23:27:16 +0700

    Create index.php

---
+++ application/views/admin/coba/index.php
@@ -0,0 +1,44 @@
+<div class="row">
+  <div class="col-xs-12">
+    <div class="card card-banner card-chart card-green no-br">
+      <div class="card-header">
+        <div class="card-title">
+          <div class="title">Top Sale Today</div>
+        </div>
+        <ul class="card-action">
+          <li>
+            <a href="/">
+              <i class="fa fa-refresh"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div class="ct-chart-sale"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+      <a class="card card-banner card-green-light">
+  <div class="card-body">
+    <i class="icon fa fa-shopping-basket fa-4x"></i>
+    <div class="content">
+      <div class="title">Sale Today</div>
+      <div class="value"><span class="sign">$</span>420</div>
+    </div>
+  </div>
+</a>
+
+  </div>
+  <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+      <a class="card card-banner card-blue-light">
+  <div class="card-body">
+    <i class="icon fa fa-thumbs-o-up fa-4x"></i>
+    <div class="content">
+      <div class="title">Page Likes</div>
+      <div class="value"><span class="sign"></span>2453</div>
+    </div>
+  </div>
+</a>
