ac10bce1c5fae476ff732e2b3bf54a2ff77179e4
Delfi Arsita <24351719+thelvie@users.noreply.github.com>
2019-09-09 08:16:58 +0700

    Create index.php

---
+++ application/views/admin/part/home/index.php
@@ -0,0 +1,100 @@
+
+<div class="row">
+  <div class="col-xs-12">
+    <div class="card card-banner card-chart card-green no-br">
+      <div class="card-header">
+        <div class="card-title">
+          <div class="title">Top Sale Today</div>
+        </div>
+        <ul class="card-action">
+          <li>
+            <a href="/">
+              <i class="fa fa-refresh"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div class="ct-chart-sale"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+      <a class="card card-banner card-green-light">
+  <div class="card-body">
+    <i class="icon fa fa-shopping-basket fa-4x"></i>
+    <div class="content">
+      <div class="title">Sale Today</div>
+      <div class="value"><span class="sign">$</span>420</div>
+    </div>
+  </div>
+</a>
+
+  </div>
+  <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+      <a class="card card-banner card-blue-light">
+  <div class="card-body">
+    <i class="icon fa fa-thumbs-o-up fa-4x"></i>
+    <div class="content">
+      <div class="title">Page Likes</div>
+      <div class="value"><span class="sign"></span>2453</div>
+    </div>
+  </div>
+</a>
+
+  </div>
+  <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+      <a class="card card-banner card-yellow-light">
+  <div class="card-body">
+    <i class="icon fa fa-user-plus fa-4x"></i>
+    <div class="content">
+      <div class="title">New Registration</div>
+      <div class="value"><span class="sign"></span>50</div>
+    </div>
+  </div>
+</a>
+
+  </div>
+</div>
+
+
+<div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+        <a class="card card-banner card-green-light">
+            <div class="card-body">
+                <i class="icon fa fa-shopping-basket fa-4x"></i>
+                <div class="content">
+                    <div class="title">Pemesanan Pending</div>
+                    <div class="value"><span class="sign"><?php echo $pend;?></span></div>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+        <a class="card card-banner card-blue-light">
+            <div class="card-body">
+                <i class="icon fa fa-thumbs-o-up fa-4x"></i>
+                <div class="content">
+                    <div class="title">Konfirmasi Pemesanan</div>
+                    <div class="value"><span class="sign"><?php echo $pend;?></span></div>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+        <a class="card card-banner card-yellow-light">
+            <div class="card-body">
+                <i class="icon fa fa-user-plus fa-4x"></i>
+                <div class="content">
+                    <div class="title">Pemesanan Dibatalkan</div>
+                    <div class="value"><span class="sign"><?php echo $cancel;?></span></div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
+
